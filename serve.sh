138aced1d8e219bda872c222faaa911b90781456
George Moura <gwmoura@gmail.com>
2015-05-28 20:13:57 -0300

    add php_runtime

--- serve.sh
+++ serve.sh
@@ -1,1 +1,1 @@
-dev_appserver.py default/app.yaml feed/feed.yaml curriculum/curriculum.yaml
+dev_appserver.py --php_executable_path="/usr/bin/php-cgi" default/app.yaml feed/feed.yaml curriculum/curriculum.yaml
