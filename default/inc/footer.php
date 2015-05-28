4b75ba4c522a70e12a6a8fcaaab5d3a437483ed7
George Moura <gwmoura@gmail.com>
2015-05-28 16:46:47 -0300

    add inc folder and mod_rewrite.php

---
+++ default/inc/footer.php
@@ -0,0 +1,14 @@
+		<footer>
+			<center>
+				<a href="http://www.facebook.com/gwmoura" target="_blank"><img src="http://www.georgemoura.com.br/imagens/icon-facebook.png"  alt="Facebook"  /></a>
+				<a href="https://github.com/gwmoura" target="_blank"><img src="http://www.georgemoura.com.br/imagens/icon-github.png"  alt="Github"  /></a>
+				<a href="http://www.twitter.com/georgemoura" target="_blank"><img src="http://www.georgemoura.com.br/imagens/icon-twitter.png"  alt="Twitter"  /></a>
+			</center>
+			<hr/>
+			<div>
+				<center>&copy; George Moura - 2015</center>
+			</div>
+		</footer>
+	
+	</body>
+</html>
---
+++ default/inc/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>George Moura - Desenvolvedor Web e Mobile</title>
+	<link rel="canonical" href="http://georgemoura.com.br/" />
+	<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-38960674-1']);
+	_gaq.push(['_trackPageview']);
+	(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+	</script>
+</head>
+<body>
+	<header>
+		<a class="home-link" href="/" title="George Moura" rel="home">
+			<h1 class="site-title">George Moura</h1>
+		</a>
+		<h4 class="site-description">Programador PHP, Ruby, Java, Javascript e Mobile</h4>
+	</header>
