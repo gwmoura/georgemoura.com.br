ce33bce52fe85ca22c01cb1a8abf14859a2911ee
George Moura <gwmoura@gmail.com>
2015-05-28 16:47:01 -0300

    add gorila lib

--- feed/feed.go
+++ feed/feed.go
@@ -1,24 +1,52 @@
-package hello
+package feed
 
 import (
 	"fmt"
-	"html/template"
-	"io/ioutil"
+	"github.com/gorilla/feeds"
 	"net/http"
+	"time"
 )
 
 func init() {
-	http.HandleFunc("/", root)
-	http.HandleFunc("/sign", sign)
+	http.HandleFunc("/", handler)
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
-}
+func handler(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
 
-func getPost(string postname) {
-	files, _ := ioutil.ReadDir("../posts/")
-	for _, f := range files {
-		fmt.Println(f.Name())
+	feed := &feeds.Feed{
+		Title:       "George Moura Blog",
+		Link:        &feeds.Link{Href: "http://georgemoura.com.br"},
+		Description: "Programador PHP, Ruby, Java, Javascript e Mobile",
+		Author:      &feeds.Author{"George Moura", "george@georgemoura.com.br"},
+		Created:     now,
 	}
+
+	feed.Items = []*feeds.Item{
+		&feeds.Item{
+			Title:       "Limiting Concurrency in Go",
+			Link:        &feeds.Link{Href: "http://jmoiron.net/blog/limiting-concurrency-in-go/"},
+			Description: "A discussion on controlled parallelism in golang",
+			Author:      &feeds.Author{"Jason Moiron", "jmoiron@jmoiron.net"},
+			Created:     now,
+		},
+		&feeds.Item{
+			Title:       "Logic-less Template Redux",
+			Link:        &feeds.Link{Href: "http://jmoiron.net/blog/logicless-template-redux/"},
+			Description: "More thoughts on logicless templates",
+			Created:     now,
+		},
+		&feeds.Item{
+			Title:       "Idiomatic Code Reuse in Go",
+			Link:        &feeds.Link{Href: "http://jmoiron.net/blog/idiomatic-code-reuse-in-go/"},
+			Description: "How to use interfaces <em>effectively</em>",
+			Created:     now,
+		},
+	}
+	rss, err := feed.ToRss()
+	if err != nil {
+		fmt.Fprint(w, "Error!")
+	}
+
+	fmt.Fprint(w, rss)
 }
