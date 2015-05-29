28ce676dd64cea39cb3589b7eaa1cb59b25095da
George Moura <gwmoura@gmail.com>
2015-05-29 10:59:57 -0300

    add keywords and author

--- default/inc/header.php
+++ default/inc/header.php
@@ -3,6 +3,9 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width">
+	<meta name="description" content="<?php echo $meta_description; ?>">
+	<meta name="keywords" content="HTML,CSS,XML,JavaScript,PHP,Ruby,Python,Go,Java,<?php echo $meta_keywords; ?>">
+	<meta name="author" content="George Moura">
 	<title>George Moura - Desenvolvedor Web e Mobile</title>
 	<link rel="canonical" href="http://georgemoura.com.br/" />
 	<link type="text/css" rel="stylesheet" href="/css/style.css" />
@@ -22,7 +25,7 @@
 		<a class="home-link" href="/" title="George Moura" rel="home">
 			<h1 class="site-title">George Moura</h1>
 		</a>
-		<h4 class="site-description">Programador PHP, Ruby, Java, Javascript e Mobile</h4>
+		<h4 class="site-description">Programador Web e Mobile</h4>
 	</header>
 	<div class="menu">
 		<a href="/" title="Início">Início</a> | 
