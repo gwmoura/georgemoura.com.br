c5222ababa56f3ba76bd7dc5032b95dfd3aaab27
George Moura <gwmoura@gmail.com>
2016-08-28 00:05:54 -0300

    add more dependencies

--- scripts/get_dependencies.sh
+++ scripts/get_dependencies.sh
@@ -1,1 +1,3 @@
+go get google.golang.org/appengine/urlfetch
+go get google.golang.org/appengine
 go get -v ./
--- scripts/install_go_appengine.sh
+++ scripts/install_go_appengine.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 wget "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-$APPENGINE_VERSION.zip" -nv
 unzip -q "go_appengine_sdk_linux_amd64-$APPENGINE_VERSION.zip"
-export GO_APPENGINE="$(pwd)/go_appengine/"
+export GO_APPENGINE="$(pwd)/go_appengine"
