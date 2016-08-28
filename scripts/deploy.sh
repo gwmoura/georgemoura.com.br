c04c9f53f975711647f114885f5014017af136bf
George Moura <gwmoura@gmail.com>
2016-08-27 22:00:04 -0300

    more one test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-appcfg.py -v --version=`date '+%Y%m%dt%H%M%S'` --application="george-moura-site" update ./
+$GO_APPENGINE/appcfg.py -v --version=`date '+%Y%m%dt%H%M%S'` --application="george-moura-site" update ./
