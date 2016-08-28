0ab436bc7efc7273775191bd2f56a7a0844e231f
George Moura <gwmoura@gmail.com>
2016-08-28 08:51:07 -0300

    update build script to fix travis build

--- scripts/install_go_appengine.sh
+++ scripts/install_go_appengine.sh
@@ -2,3 +2,4 @@
 wget "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-$APPENGINE_VERSION.zip" -nv
 unzip -q "go_appengine_sdk_linux_amd64-$APPENGINE_VERSION.zip"
 export GO_APPENGINE="$(pwd)/go_appengine"
+export APPENGINE_DEV_APPSERVER=$(pwd)/go_appengine/dev_appserver.py
