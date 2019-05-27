1a6adb63aad00b4f99096c265061f5079fb0f374
George Moura <gwmoura@gmail.com>
2019-05-27 00:01:27 -0300

    updating deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-# source scripts/install_go_appengine.sh
-
-# install Go component
-# ./google-cloud-sdk/bin/gcloud components install app-engine-go -q
-# ./google-cloud-sdk/platform/google_appengine/appcfg.py --oauth2_refresh_token=$GAE_OAUTH -v --version=`date '+%Y%m%dt%H%M%S'` --application="george-moura-site" update ./site/app.yaml
-gcloud app deploy site/app.yaml --project george-moura-site
+gcloud app deploy --promote site/app.yaml --project george-moura-site
